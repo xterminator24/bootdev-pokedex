3b4a4e947ff1d96473e6aae17aa071d98bc8cf58
xterminator24 <scott.mckillop@outlook.com>
2025-03-08 20:30:23 -0400

    refactor explore command to use GetLocation method and update location data structures

--- command_explore.go
+++ command_explore.go
@@ -6,15 +6,18 @@ import (
 )
 
 func commandExplore(config *Config, args ...string) error {
-	if len(args) == 0 {
+	if len(args) != 1 {
 		return errors.New("you must provide a location area name")
 	}
 
 	requestedArea := args[0]
-	locationArea, err := config.pokeapiClient.Explore(requestedArea)
+	locationArea, err := config.pokeapiClient.GetLocation(requestedArea)
 	if err != nil {
 		return errors.New("area doesn't exist")
 	}
+
+	fmt.Printf("Exploring %s...\n", locationArea.Name)
+	fmt.Printf("Found Pokemon: \n")
 
 	for _,encounter := range locationArea.PokemonEncounters {
 		fmt.Println(encounter.Pokemon.Name)
--- internal/pokeapi/location_explore.go
+++ internal/pokeapi/location_explore.go
@@ -7,42 +7,42 @@ import (
 	"net/http"
 )
 
-func (c *Client) Explore(locationName string) (RespLocationArea, error) {
+func (c *Client) GetLocation(locationName string) (Location, error) {
 	url := baseURL + "/location-area/" + locationName
 
 	// If cache exists for the request use it and return
 	if cachedData, found := c.Cache.Get(url); found {
 		fmt.Println("using cache...")
-		var locationArea	RespLocationArea
-		err := json.Unmarshal(cachedData, &locationArea)
-		return locationArea, err
+		var location	Location
+		err := json.Unmarshal(cachedData, &location)
+		return location, err
 	}
 	
 	// Otherwise fetch the data from the API
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return RespLocationArea{}, err
+		return Location{}, err
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return RespLocationArea{}, err
+		return Location{}, err
 	}
 	defer resp.Body.Close()
 
 	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return RespLocationArea{}, err
+		return Location{}, err
 	}
 
 	// add the response data to the cache
 	c.Cache.Add(url, dat)
 
-	locationAreaResp := RespLocationArea{}
-	err = json.Unmarshal(dat, &locationAreaResp)
+	location := Location{}
+	err = json.Unmarshal(dat, &location)
 	if err != nil {
-		return RespLocationArea{}, err
+		return Location{}, err
 	}
 
-	return locationAreaResp, nil
+	return location, nil
 }
